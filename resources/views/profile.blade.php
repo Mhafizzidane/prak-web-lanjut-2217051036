bb1e3ae7bcb1aaf962d545ce6fa8a8b5e36d4189
Mhafizzidane <hfzzidane27@gmail.com>
2024-09-09 16:17:15 +0700

    perubahan profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,25 +3,77 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile Page</title>
+    <title>Profile Saya</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-image: url('{{ asset('images/valo.png') }}');
+            background-size: cover;
+            background-position: center;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            backdrop-filter: blur(3px);
+            font-family: 'Times New Roman', serif;
+            margin: 0;
+        }
+
+        .profile-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            background-color: rgba(255, 255, 255, 0.8);     
+            border-radius: 10px;
+            padding: 20px;
+            box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1); 
+        }
+
+        .profile-image {
+            border-radius: 50%;
+            border: 2px solid #ccc;
+            width: 200px;
+            height: 200px;
+            background-color: #f0f0f0;
+            overflow: hidden;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .profile-image img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover; 
+        }
+        .profile-info {
+            margin-top: 20px;
+            width: 300px;
+        }
+        .profile-info div {
+            background-color: #696969;
+            color: #fff; 
+            text-align: center;
+            padding: 10px;
+            margin-bottom: 10px;
+            border-radius: 5px;
+        }
+    </style>
 </head>
 <body>
-<table>
-    <tr>
-        <td>Nama</td>
-        <td>:</td>
-        <td><?= $nama ?></td>
-    </tr>
-    <tr>
-        <td>Kelas</td>
-        <td>:</td>
-        <td><?= $kelas ?></td>
-    </tr>
-    <tr>
-        <td>NPM</td>
-        <td>:</td>
-        <td><?= $npm ?></td>
-    </tr>
-</table>
+    <div class="container">
+        <div class="profile-container">
+            <div class="profile-image">
+                <img src="{{ asset('images/foto_profile.JPG') }}" alt="Profile Image"> <!-- Gambar profil -->
+            </div>
+
+            <div class="profile-info">
+                <div>Nama: <?= $nama ?></div>
+                <div>Kelas: <?= $kelas ?></div>
+                <div>NPM: <?= $npm ?></div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
